34d481a96a4dcd7fe99e50716e1b3e113b8bd196
Kosynskyi <kosynskyi.vova@gmail.com>
2022-06-15 17:46:46 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,10 @@ const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", (event) => {
-  inputEl !== ""
-    ? (outputEl.textContent = event.currentTarget.value)
-    : "Anonymous";
+  outputEl.textContent =
+    event.currentTarget.value === "" ? "Anonymous" : event.currentTarget.value;
+
+  // inputEl !== ""
+  //   ? (outputEl.textContent = event.currentTarget.value)
+  //   : "Anonymous";
 });
